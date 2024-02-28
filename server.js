2bf728a97fda8940eac327febf0468adde7920c4
chris-diam <ch.diamantakis@gmail.com>
2024-02-28 15:15:39 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+const houses = require("./houses");
+
+app.get("/houses", (req, res) => {
+  const { name } = req.query;
+  if (name) {
+    const filteredHouses = houses.filter((house) =>
+      house.name.toLowerCase().includes(name.toLowerCase())
+    );
+    res.json(filteredHouses);
+  } else {
+    res.json(houses);
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
